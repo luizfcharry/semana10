a7f026078b09fff4062c00df5714512d73c11985
Luiz Felipe de Camargo <camargo.luizfelipe@gmail.com>
2020-01-15 22:59:41 -0300

    fim do terceiro dia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://semana:semana123@cluster0-0yvj0.mongodb.net/semana10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-//teste
 
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -1,84 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
 
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://s2.glbimg.com/rRsNxJQKU9ZuKK6MFbztcdj-D4U=/186x105/top/smart/filters:strip_icc()/s2.glbimg.com/eYLCIEb_NRyuGhwlDjLoB63wOTE=/574x110:841x260/267x150/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/A/C/DykZzlRIqciBupQzSOdQ/jfcrz-150120jcrz74835572.jpg"/>
-              <div className="user-info">
-                <strong>Jair Bolsonaro</strong>
-                <span>Java, React, Node.js</span>
-              </div>
-            </header>
-            <p>Ta ok? Paulo Guedes</p>
-            <a href="https://github.com/luizfcharry">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://s2.glbimg.com/rRsNxJQKU9ZuKK6MFbztcdj-D4U=/186x105/top/smart/filters:strip_icc()/s2.glbimg.com/eYLCIEb_NRyuGhwlDjLoB63wOTE=/574x110:841x260/267x150/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/A/C/DykZzlRIqciBupQzSOdQ/jfcrz-150120jcrz74835572.jpg"/>
-              <div className="user-info">
-                <strong>Jair Bolsonaro</strong>
-                <span>Java, React, Node.js</span>
-              </div>
-            </header>
-            <p>Ta ok? Paulo Guedes</p>
-            <a href="https://github.com/luizfcharry">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://s2.glbimg.com/rRsNxJQKU9ZuKK6MFbztcdj-D4U=/186x105/top/smart/filters:strip_icc()/s2.glbimg.com/eYLCIEb_NRyuGhwlDjLoB63wOTE=/574x110:841x260/267x150/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/A/C/DykZzlRIqciBupQzSOdQ/jfcrz-150120jcrz74835572.jpg"/>
-              <div className="user-info">
-                <strong>Jair Bolsonaro</strong>
-                <span>Java, React, Node.js</span>
-              </div>
-            </header>
-            <p>Ta ok? Paulo Guedes</p>
-            <a href="https://github.com/luizfcharry">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://s2.glbimg.com/rRsNxJQKU9ZuKK6MFbztcdj-D4U=/186x105/top/smart/filters:strip_icc()/s2.glbimg.com/eYLCIEb_NRyuGhwlDjLoB63wOTE=/574x110:841x260/267x150/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/A/C/DykZzlRIqciBupQzSOdQ/jfcrz-150120jcrz74835572.jpg"/>
-              <div className="user-info">
-                <strong>Jair Bolsonaro</strong>
-                <span>Java, React, Node.js</span>
-              </div>
-            </header>
-            <p>Ta ok? Paulo Guedes</p>
-            <a href="https://github.com/luizfcharry">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
