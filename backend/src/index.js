f05911d60d17a19dd214d9f5cd6a0b3372a77087
Luiz Felipe de Camargo <camargo.luizfelipe@gmail.com>
2020-01-17 13:04:47 -0300

    fim da semana

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://semana:semana123@cluster0-0yvj0.mongodb.net/semana10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -11,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewsDevs } from '../services/socket';
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
@@ -35,7 +36,24 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewsDevs(dev => setDevs([...devs, dev]));
+    }, [devs]);
+
+
+    function setupWebSocket() {
+        const { latitude, longitude } = currentRegion;
+
+        connect(
+            latitude,
+            longitude,
+            techs,
+        );
+    };
+
     async function loadDevs() {
+        disconnect();
+
         const { latitude, longitude } = currentRegion;
 
         const response = await api.get('/search', {
@@ -47,6 +65,7 @@ function Main({ navigation }) {
         });
 
         setDevs(response.data);
+        setupWebSocket();
 
     }
 
